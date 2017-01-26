a8e3fd1cdb5085fc8a52a33469d31e1b06b81f97
PiratKrabe <Link_Ex@i.ua>
2017-01-26 21:59:13 +0200

    Basic

---
+++ SecondClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Linkin on 26.01.2017.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("My first programs on Java...");
+    }
+}
